bca5b0d8cef90ec893d8aa9aac091c871610c6a4
N1kolozz <mr.osievi5@gmail.com>
2025-04-18 00:12:04 +0400

    Final-Project-starter-code

---
+++ src/app/(main)/products/details/[id]/page.jsx
@@ -0,0 +1,51 @@
+import styles from "./page.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+
+const ProductDetails = async ({ params }) => {
+
+  const id = params.id;
+  let response;
+
+  try {
+    response = await fetch(`https://fakestoreapi.com/products/${id}`);
+  } catch (error) {
+    throw Error("Error fetching product data", error);
+  }
+ 
+  const product = await response.json();
+
+
+
+  return (
+    <section className={styles.container}>
+      <div className={styles.itemContainer}>
+      <div>
+        <div className={styles.imageContainer}>
+          <Image src={product.image} width={200} height={200} alt="product-image"/>
+        </div>
+        <h3 className={styles.title}>{product.title}</h3>
+      </div>
+      <div>
+        <p className={styles.desc}>{product.description}</p>
+      </div>
+      <div className={styles.priceContainer}>
+        <h4 className={styles.price}>{product.price}$</h4>
+        <div className={styles.ratingWrapper}>
+            <p className={styles.rating}>{product.rating.rate}/5</p>
+            <p className={styles.ratingCount}>{product.rating.count} reviews</p>
+        </div>
+      </div>
+      <button className={styles.addToCart}>Add to cart</button>
+    </div>
+    <Link href="/products">
+      <button className={styles.goBack}>Go back</button>
+      </Link>
+    </section>
+    
+    
+  );
+};
+
+export default ProductDetails;
---
+++ src/app/(main)/products/page.jsx
@@ -0,0 +1,30 @@
+import ProductItem from "@/components/ProductItem/ProductItem";
+import styles from "./page.module.css";
+import Link from "next/link";
+import { headers } from "next/headers";
+
+
+const Products = async () => {
+  const headersList = await headers();
+  const path = headersList.get("x-next-url");
+  let products;
+  try {
+    const data = await fetch("https://fakestoreapi.com/products");
+    products = await data.json();
+  } catch (error) {
+    throw Error(error);
+  }
+  console.log(path);
+
+  return (
+    <div className={styles.container}>
+      {products.map((product) => (
+        <Link key={product.id} href={`/products/details/${product.id}`}>
+          <ProductItem item={product} />
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Products;
