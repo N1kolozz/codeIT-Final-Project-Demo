3d836c08ea67adb0dce64b8535ae2231ae6f5406
N1kolozz <mr.osievi5@gmail.com>
2025-05-09 20:47:36 +0400

    Refactor loading components to use Image from next/image and improve loading UI with SVG animation

--- src/app/(main)/profile/loading.jsx
+++ src/app/(main)/profile/loading.jsx
@@ -1,7 +1,11 @@
 "use client";
+import Image from "next/image";
 
 function loading() {
-  return <div>loading...</div>;
+  return <div>
+    <Image src={"/assets/images/loading.svg"} width={100} height={100} alt="loading" />
+    <h2>Loading...</h2>
+  </div>;
 }
 
 export default loading;
