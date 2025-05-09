ffbdc4c3d4795b9133d46eea6e80cadb983d8abf
N1kolozz <mr.osievi5@gmail.com>
2025-05-09 22:05:03 +0400

    Refactor loading components to use consistent loadingStyles and enhance layout for better responsiveness

--- src/app/(main)/profile/loading.jsx
+++ src/app/(main)/profile/loading.jsx
@@ -1,8 +1,9 @@
 "use client";
 import Image from "next/image";
+import styles from "./page.module.css";
 
 function loading() {
-  return <div>
+  return <div className={styles.loadingStyles}>
     <Image src={"/assets/images/loading.svg"} width={100} height={100} alt="loading" />
     <h2>Loading...</h2>
   </div>;
